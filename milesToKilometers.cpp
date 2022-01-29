1d6d1a6fe75b40e87f297652e91468925880bddb
Daniel Ibanescu <ohmyzazu@gmail.com>
2022-01-29 16:32:15 -0500

    some simple numbers conversion

---
+++ milesToKilometers.cpp
@@ -0,0 +1,14 @@
+//
+// Created by zsazu on 2022-01-29.
+//
+#include <iostream>
+using namespace std;
+int main(){
+  int miles;
+  float kilometers;
+  cout << "Enter the distance in miles: " << endl;
+  cin >> miles;
+  float conversionValue = 1.609344;
+  kilometers = miles * conversionValue;
+  cout << "The distance is " << kilometers << " kilometers" << endl;
+}
